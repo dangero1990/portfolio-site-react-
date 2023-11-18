b8535665cac9633b76ec69d387ad19108467ab0b
Andrew Eubanks <aeubanksguitar@gmail.com>
2023-11-18 11:41:57 -0500

    added error handling for form submission

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,9 +3,14 @@ import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import emailjs from '@emailjs/browser';
 import useTypewritter from '../hooks/useTypewriter';
+import { useState } from 'react';
 
 function Contact() {
-  const text = useTypewritter("Send me a message! I'd love to hear from you");
+  const text = useTypewritter("Send me a message. I'd love to hear from you!");
+  const successMessage = useTypewritter('Thank you for your message. I will be sure to reach back out to you soon :)');
+  const errorMessage = useTypewritter('Something went wrong. Please try again later.');
+  const [sent, setSent] = useState(false);
+  const [message, setMessage] = useState('');
 
   const formik = useFormik({
     initialValues: {
@@ -19,28 +24,47 @@ function Contact() {
       message: Yup.string().required('Required'),
     }),
     onSubmit: (values, { resetForm }) => {
-      console.log(process.env.REACT_APP_testEnv);
-      emailjs.send(process.env.REACT_APP_serviceID, process.env.REACT_APP_templateID, values, process.env.REACT_APP_publicKey);
-      resetForm({ values: '' });
+      emailjs
+        .send(process.env.REACT_APP_serviceID, process.env.REACT_APP_templateID, values, process.env.REACT_APP_publicKey)
+        .then(() => {
+          setSent(true);
+          setMessage(successMessage);
+          resetForm({ values: '' });
+        })
+        .catch((error) => {
+          if (error.status !== 200) {
+            setSent(true);
+            setMessage(errorMessage);
+          }
+        });
     },
   });
 
   return (
     <section className='contact'>
       <h2>Contact Me</h2>
-      <p>{text}</p>
-      <form onSubmit={formik.handleSubmit}>
-        <label htmlFor='name'>Name</label>
-        <input id='name' name='name' type='text' onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.name} placeholder='Your Name' />
-        {formik.touched.name && formik.errors.name ? <div className='required'>{formik.errors.name}</div> : null}
-        <label htmlFor='email'>Email</label>
-        <input id='email' name='email' type='email' onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.email} placeholder='email@email.com' />
-        {formik.touched.email && formik.errors.email ? <div className='required'>{formik.errors.email}</div> : null}
-        <label htmlFor='message'>message</label>
-        <textarea name='message' id='message' cols='30' rows='10' onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.message} placeholder="Hello, Andrew I'd like to talk" />
-        {formik.touched.email && formik.errors.email ? <div className='required'>{formik.errors.message}</div> : null}
-        <button type='submit'>Submit</button>
-      </form>
+      {!sent && (
+        <>
+          <p>{text}</p>
+          <form onSubmit={formik.handleSubmit}>
+            <label htmlFor='name'>Name</label>
+            <input id='name' name='name' type='text' onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.name} placeholder='Your Name' />
+            {formik.touched.name && formik.errors.name ? <div className='required'>{formik.errors.name}</div> : null}
+            <label htmlFor='email'>Email</label>
+            <input id='email' name='email' type='email' onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.email} placeholder='email@email.com' />
+            {formik.touched.email && formik.errors.email ? <div className='required'>{formik.errors.email}</div> : null}
+            <label htmlFor='message'>message</label>
+            <textarea name='message' id='message' cols='30' rows='10' onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.message} placeholder="Hello, Andrew I'd like to talk" />
+            {formik.touched.email && formik.errors.email ? <div className='required'>{formik.errors.message}</div> : null}
+            <button type='submit'>Submit</button>
+          </form>
+        </>
+      )}
+      {sent && (
+        <>
+          <p className='status-message'>{message}</p>
+        </>
+      )}
       <Link to={'/home'} id='home'>
         go home
       </Link>
