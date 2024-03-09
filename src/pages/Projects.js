7a081fa3db67daaa13ac4ce14ffae2c8531091c1
Andrew <aeubanksguitar@gmail.com>
2024-03-09 10:38:58 -0500

    added new project

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -4,6 +4,7 @@ import Card from '../components/Card';
 import solver from '../assets/solvher.png';
 import ageCalc from '../assets/ageCalc.png';
 import flixx from '../assets/flixx.png';
+import staticJobListing from '../assets/staticJobListing.png';
 import adviceGenerator from '../assets/adviceGenerator.png';
 import MuteButton from '../components/MuteButton';
 
@@ -15,13 +16,42 @@ function Projects() {
       <p>{text}</p>
       <MuteButton />
       <div className='card-grid'>
-        <Card img={solver} header='solvher' description='a web app for cycle counting' link='http://solvher.net' />
-        <Card img={ageCalc} header='age calculator' description='a web app built to calculate age down to the day' link='https://timely-dusk-9b848e.netlify.app/' />
-        <Card img={flixx} header='flixx' description='a website for finding out information about your favorite movie or tv shows' link='https://aesthetic-dodol-1da2e6.netlify.app/' />
-        <Card img={adviceGenerator} header='advice generator' description='a web app for generatoring random life advice' link='https://precious-treacle-42b54b.netlify.app/' />
+        <Card
+          img={solver}
+          header='solvher'
+          description='a web app for cycle counting'
+          link='http://solvher.net'
+        />
+        <Card
+          img={ageCalc}
+          header='age calculator'
+          description='a web app built to calculate age down to the day'
+          link='https://timely-dusk-9b848e.netlify.app/'
+        />
+        <Card
+          img={flixx}
+          header='flixx'
+          description='a website for finding out information about your favorite movie or tv shows'
+          link='https://aesthetic-dodol-1da2e6.netlify.app/'
+        />
+        <Card
+          img={adviceGenerator}
+          header='advice generator'
+          description='a web app for generatoring random life advice'
+          link='https://precious-treacle-42b54b.netlify.app/'
+        />
+        <Card
+          img={staticJobListing}
+          header='static job listing'
+          description='a web page for filtering jobs'
+          link='https://dangero1990.github.io/static-job-listing/'
+        />
       </div>
       {!complete && <span className='skip'>press any button to skip</span>}
-      <Link to={'/home'} id='home'>
+      <Link
+        to={'/home'}
+        id='home'
+      >
         go home
       </Link>
     </section>
